05d63a3e046681950d1e1cf9bd30c391202a1657
althafvly <althafvly@gmail.com>
2020-05-11 20:04:04 +0530

    Update for pie

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@ DEVICE_PATH := device/motorola/osprey
 
 include device/motorola/msm8916-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := osprey-mini_defconfig
+TARGET_KERNEL_CONFIG := osprey_defconfig
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
